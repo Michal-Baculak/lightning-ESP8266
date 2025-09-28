bb02679af7e134ef39ff730404d80ff4640e6a79
Michal Baculák <michal.baculak@gmail.com>
2025-09-28 21:57:15 +0200

    implement soft hotspot

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,26 @@
 *********/
 
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 
 #define LED 5
+
+const char *ssid = "ESP8266 AP";
+const char *password = "YOLOisTOOshort";
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(LED, OUTPUT);
+  boolean result = WiFi.softAP(ssid, password);
+  if(result)
+    Serial.println("Access point started!");
+  else
+    Serial.println("Access point failed to start!");
+  Serial.print("IP: ");
+  Serial.println(WiFi.softAPIP());
+  Serial.print("Network is operating under SSID: ");
+  Serial.println(WiFi.softAPSSID());
 }
 
 void loop() {
@@ -21,4 +34,5 @@ void loop() {
   digitalWrite(LED, LOW);
   Serial.println("LED is off");
   delay(1000);
+  Serial.printf("Connected stations: %d\n",WiFi.softAPgetStationNum());
 }
